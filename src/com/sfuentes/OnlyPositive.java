5116d8ced3b093e3a8e5f75c9cac7903a8947b1b
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-04-21 22:25:41 -0500

    Fixe rename all methods and variable names

--- src/com/sfuentes/OnlyPositive.java
+++ src/com/sfuentes/OnlyPositive.java
@@ -3,16 +3,16 @@ package com.sfuentes;
 import java.util.ArrayList;
 
 public class OnlyPositive {
-  public ArrayList<Double> showOnlyPositiveNumbers(double[] numbers) {
+  public ArrayList<Double> filterPositiveNumbers(double[] numbers) {
 
-    ArrayList<Double> positive = new ArrayList<>();
+    ArrayList<Double> positives = new ArrayList<>();
     int i = 0;
 
     for (double number : numbers)
       if (number > 0) {
-        positive.add(number);
+        positives.add(number);
       }
-    return positive;
+    return positives;
   }
 }
 
--- src/com/sfuentes/OnlyPositiveTest.java
+++ src/com/sfuentes/OnlyPositiveTest.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 
 public class OnlyPositiveTest {
   public static void main(String[] args) {
-    showOnlyPositiveNumbers_positiveIsCorrect();
-    showOnlyPositiveNumbers_Zero();
-    showOnlyPositiveNumbers_Negative();
-    showOnlyPositiveNumbers_sameNumbersPositive();
+    filterPositiveNumbers_positiveIsCorrect();
+    filterPositiveNumbers_Zero();
+    filterPositiveNumbers_Negative();
+    filterPositiveNumbers_sameNumbersPositive();
   }
 
-  public static void showOnlyPositiveNumbers_positiveIsCorrect() {
+  public static void filterPositiveNumbers_positiveIsCorrect() {
     OnlyPositive correct = new OnlyPositive();
 
     double[] numbers = {1.5, 0, -9, 7.8, -4, -3, -2, -8, -9, -10};
     ArrayList<Double> expectedResult = new ArrayList<>(Arrays.asList(1.5d, 7.8d));
-    ArrayList<Double> actualResult = correct.showOnlyPositiveNumbers(numbers);
+    ArrayList<Double> actualResult = correct.filterPositiveNumbers(numbers);
 
     System.out.println("Test case 1: Show positive numbers correct: \n\t" +
         "Test numbers: " + Arrays.toString(numbers) + " \n\t" +
@@ -28,52 +28,52 @@ public class OnlyPositiveTest {
       System.out.println("\tTEST FAILED.");
   }
 
-  public static void showOnlyPositiveNumbers_Zero() {
+  public static void filterPositiveNumbers_Zero() {
     OnlyPositive zero = new OnlyPositive();
 
-    double[] numbers2 = {0, 0, 0, 0, 0};
-    ArrayList<Double> expectedResult2 = new ArrayList<>(Arrays.asList());
-    ArrayList<Double> actualResult2 = zero.showOnlyPositiveNumbers(numbers2);
+    double[] numbers = {0, 0, 0, 0, 0};
+    ArrayList<Double> expectedResult = new ArrayList<>(Arrays.asList());
+    ArrayList<Double> actualResult = zero.filterPositiveNumbers(numbers);
 
     System.out.println("Test case 2: test with all the numbers in zero: \n\t" +
-        "Test numbers: " + Arrays.toString(numbers2) + " \n\t" +
-        "Expected result:" + expectedResult2 + " \n\t" +
-        "Actual result: " + actualResult2);
-    if (actualResult2.equals(expectedResult2))
+        "Test numbers: " + Arrays.toString(numbers) + " \n\t" +
+        "Expected result:" + expectedResult + " \n\t" +
+        "Actual result: " + actualResult);
+    if (actualResult.equals(expectedResult))
       System.out.println("\tSUCCESSFUL TEST.");
     else
       System.out.println("\tTEST FAILED.");
   }
 
-  public static void showOnlyPositiveNumbers_Negative() {
+  public static void filterPositiveNumbers_Negative() {
     OnlyPositive negative = new OnlyPositive();
 
-    double[] numbers3 = {-100.0, -5000.0, -90.0, -99.0};
-    ArrayList<Double> expectedResult3 = new ArrayList<>(Arrays.asList());
-    ArrayList<Double> actualResult3 = negative.showOnlyPositiveNumbers(numbers3);
+    double[] numbers = {-100.0, -5000.0, -90.0, -99.0};
+    ArrayList<Double> expectedResult = new ArrayList<>(Arrays.asList());
+    ArrayList<Double> actualResult = negative.filterPositiveNumbers(numbers);
 
     System.out.println("Test case 3: test with all negative numbers: \n\t" +
-        "Test numbers: " + Arrays.toString(numbers3) + " \n\t" +
-        "Expected result:" + expectedResult3 + " \n\t" +
-        "Actual result: " + actualResult3);
-    if (actualResult3.equals(expectedResult3))
+        "Test numbers: " + Arrays.toString(numbers) + " \n\t" +
+        "Expected result:" + expectedResult + " \n\t" +
+        "Actual result: " + actualResult);
+    if (actualResult.equals(expectedResult))
       System.out.println("\tSUCCESSFUL TEST.");
     else
       System.out.println("\tTEST FAILED.");
   }
 
-  public static void showOnlyPositiveNumbers_sameNumbersPositive() {
+  public static void filterPositiveNumbers_sameNumbersPositive() {
     OnlyPositive samePositive = new OnlyPositive();
 
     double[] numbers = {10.0, 10.0, 10.0, 10.0, 10.0, 10.0};
-    ArrayList<Double> expectedResult4 = new ArrayList<>(Arrays.asList(10.0d, 10.0d, 10.0d, 10.0d, 10.0d, 10.0d));
-    ArrayList<Double> actualResult4 = samePositive.showOnlyPositiveNumbers(numbers);
+    ArrayList<Double> expectedResult = new ArrayList<>(Arrays.asList(10.0d, 10.0d, 10.0d, 10.0d, 10.0d, 10.0d));
+    ArrayList<Double> actualResult = samePositive.filterPositiveNumbers(numbers);
 
     System.out.println("Test case 4: test with all positive numbers: \n\t" +
         "Test numbers: " + Arrays.toString(numbers) + " \n\t" +
-        "Expected result:" + expectedResult4 + " \n\t" +
-        "Actual result: " + actualResult4);
-    if (actualResult4.equals(expectedResult4))
+        "Expected result:" + expectedResult + " \n\t" +
+        "Actual result: " + actualResult);
+    if (actualResult.equals(expectedResult))
       System.out.println("\tSUCCESSFUL TEST.");
     else
       System.out.println("\tTEST FAILED.");
