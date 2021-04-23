afbeead17ab5971b2856b676b872667bb05798e8
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-04-23 16:09:40 -0500

    Add NegativeToPositive and NegativeToPositiveTest files

---
+++ src/com/sfuentes/NegativeToPositive.java
@@ -0,0 +1,16 @@
+package com.sfuentes;
+
+import java.util.ArrayList;
+
+public class NegativeToPositive {
+  public ArrayList<Double> convertNegativeToPositive(double[] numbers) {
+
+    ArrayList<Double> positives = new ArrayList<>();
+    for (double number : numbers) {
+      if (number < 0)
+        number = number * (-1);
+      positives.add(number);
+    }
+    return positives;
+  }
+}
