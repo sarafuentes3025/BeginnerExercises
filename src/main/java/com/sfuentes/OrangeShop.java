d454e47c765c808399b7467cb7d1c0499bd37106
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-05-10 15:15:49 -0500

    format code

--- src/main/java/com/sfuentes/OrangeShop.java
+++ src/main/java/com/sfuentes/OrangeShop.java
@@ -2,19 +2,19 @@ package com.sfuentes;
 
 public class OrangeShop {
 
-  public double calculateFifteenPercentage(double number){
+  public double calculateFifteenPercentage(double number) {
     return .15 * number;
   }
 
-  public double calculateCustomerPayment(double pricePerKilo,double kilos){
+  public double calculateCustomerPayment(double pricePerKilo, double kilos) {
     double total = pricePerKilo * kilos;
-    return kilos > 10 ? total- calculateFifteenPercentage(total):total;
+    return kilos > 10 ? total - calculateFifteenPercentage(total) : total;
   }
 
-  public double calculateStoreSales(double pricePerKilo,double[] kilos){
+  public double calculateStoreSales(double pricePerKilo, double[] kilos) {
     double total = 0;
-    for(double kilo: kilos){
-      total += calculateCustomerPayment(pricePerKilo,kilo);
+    for (double kilo : kilos) {
+      total += calculateCustomerPayment(pricePerKilo, kilo);
     }
     return total;
   }
