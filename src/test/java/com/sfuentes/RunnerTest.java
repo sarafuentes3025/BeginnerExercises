4f90a08d2652e1eafa51996a00f16e4d6ce456cd
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-05-13 16:47:38 -0500

    Change name method in RunnerTest

--- src/test/java/com/sfuentes/RunnerTest.java
+++ src/test/java/com/sfuentes/RunnerTest.java
@@ -47,7 +47,7 @@ public class RunnerTest {
   }
 
   @Test
-  void calculateIfItIsSuitable_allBiggest() {
+  void calculateIfItIsSuitable_allGreaterThanSixteen() {
     Runner test = new Runner();
     double[] laps = {20, 30, 40, 50, 60, 10, 20, 30, 40, 50};
 
