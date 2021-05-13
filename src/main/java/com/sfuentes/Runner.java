d8e2cfb104f5086c0c80a62e4aced25734a9146e
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-05-13 16:29:49 -0500

    Optimize code

--- src/main/java/com/sfuentes/Runner.java
+++ src/main/java/com/sfuentes/Runner.java
@@ -3,24 +3,19 @@ package com.sfuentes;
 public class Runner {
 
   public boolean calculateIfItIsSuitable(double[] laps) {
-    boolean suitable;
+    boolean suitable = false;
     double sum = 0;
     int lessThanSixteen = 0;
-    int overSixteen = 0;
     for (double lap : laps) {
       if (lap < 16.0)
         lessThanSixteen++;
-      else
-        overSixteen++;
       sum += lap;
     }
 
     double average = sum / laps.length;
 
-    if (lessThanSixteen == 10 || overSixteen == 1)
+    if (lessThanSixteen >= 9 || average <= 15.0)
       suitable = true;
-    else
-      suitable = average <= 15.0;
 
     return suitable;
   }
