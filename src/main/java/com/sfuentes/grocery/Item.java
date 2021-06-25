a8b40658aca41b36b1d8f118322bdf57ccfab76e
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-06-25 16:56:46 -0500

    Install lombok dependency and use in Item Class

--- src/main/java/com/sfuentes/grocery/Item.java
+++ src/main/java/com/sfuentes/grocery/Item.java
@@ -1,20 +1,11 @@
 package com.sfuentes.grocery;
 
-public class Item {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Getter
+public class Item {
   private final String nameItem;
   private final double price;
-
-  public Item(String nameItem, double price) {
-    this.nameItem = nameItem;
-    this.price = price;
-  }
-
-  public String getNameItem() {
-    return nameItem;
-  }
-
-  public double getPrice() {
-    return price;
-  }
 }
