5a6c26d0ad52588048fc5b837789eb59b4ec9e17
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-06-26 16:45:45 -0500

    Use lombok in Client class in the grocery package

--- src/main/java/com/sfuentes/grocery/Client.java
+++ src/main/java/com/sfuentes/grocery/Client.java
@@ -1,16 +1,18 @@
 package com.sfuentes.grocery;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
 
 public class Client {
   private final List<Item> items = new ArrayList<>();
 
   public void addItem(Item item) {
     items.add(item);
-  }
-
-  public List<Item> getItems() {
-    return items;
   }
 }
