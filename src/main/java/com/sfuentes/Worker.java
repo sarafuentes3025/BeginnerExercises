d296125214eb3dea4c4de67292194eb4793331c2
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-07-03 18:51:49 -0500

    Use Lombok worker class

--- src/main/java/com/sfuentes/Worker.java
+++ src/main/java/com/sfuentes/Worker.java
@@ -1,22 +1,18 @@
 package com.sfuentes;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Worker {
 
-  int workerId;
-  int hours;
-
-  public Worker(int workerId, int hours) {
-    this.workerId = workerId;
-    this.hours = hours;
-  }
+  public final int workerId;
+  public final int hours;
 
   public double calculateSalary() {
-
     if (hours <= 40)
       return hours * 200;
     else
       return 8_000 + (hours - 40) * 250;
-
   }
 }
 
